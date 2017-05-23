40b53ffa16b58db4214abc0baa8313298d08301f
wtritch <wtritchler@fairwaytech.com>
2017-05-23 16:13:11 -0700

    Optionally stop processing poison messages.

--- src/main/java/tds/exam/results/messaging/ExamCompletedMessageListener.java
+++ src/main/java/tds/exam/results/messaging/ExamCompletedMessageListener.java
@@ -4,11 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import tds.exam.ExamStatusCode;
+import tds.exam.results.configuration.ExamResultsTransmitterServiceProperties;
 import tds.exam.results.services.ExamResultsService;
 import tds.exam.results.services.ExamService;
-import tds.exam.results.trt.TDSReport;
 
 import java.util.UUID;
 
@@ -23,11 +22,15 @@ public class ExamCompletedMessageListener {
 
     private final ExamService examService;
 
+    private final ExamResultsTransmitterServiceProperties properties;
+
     @Autowired
     public ExamCompletedMessageListener(final ExamResultsService examResultsService,
-                                        final ExamService examService) {
+                                        final ExamService examService,
+                                        final ExamResultsTransmitterServiceProperties properties) {
         this.examResultsService = examResultsService;
         this.examService = examService;
+        this.properties = properties;
     }
 
     /**
@@ -42,8 +45,23 @@ public class ExamCompletedMessageListener {
      */
     public void handleMessage(final String examId) {
         LOG.debug("Received completed exam notification for id: {}", examId);
+        if (properties.isRetryOnError()) {
+            processMessage(examId);
+        } else {
+            try {
+                processMessage(examId);
+            } catch (final Exception e) {
+                // Log error, and do not rethrow to prevent ERT from re-processing this same request
+                LOG.error("Error occurred while processing or sending the exam results for examId {}: {}", examId,
+                    e);
+            }
+        }
+
+    }
+
+    private void processMessage(final String examId) {
         // Once this ERT message has been received, the exam status is "submitted"
-        UUID id = UUID.fromString(examId);
+        final UUID id = UUID.fromString(examId);
         examService.updateStatus(id, ExamStatusCode.STATUS_SUBMITTED);
         examResultsService.findAndSendExamResults(id);
     }
