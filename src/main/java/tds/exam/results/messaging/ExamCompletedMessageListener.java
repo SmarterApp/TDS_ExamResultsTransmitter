dafe204c2a73d147b41777031c73b3e0efbb6b6d
Ernie Munoz <emunoz@fairwaytech.com>
2017-06-19 15:08:00 -0700

    Updating Copyright

--- src/main/java/tds/exam/results/messaging/ExamCompletedMessageListener.java
+++ src/main/java/tds/exam/results/messaging/ExamCompletedMessageListener.java
@@ -1,16 +1,15 @@
 /*******************************************************************************
- * Copyright 2016 Smarter Balance Licensed under the
- *     Educational Community License, Version 2.0 (the "License"); you may
- *     not use this file except in compliance with the License. You may
- *     obtain a copy of the License at
+ * Copyright 2017 Regents of the University of California. Licensed under the Educational
+ * Community License, Version 2.0 (the “license”); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the license at
  *
- * http://www.osedu.org/licenses/ECL-2.0
+ * https://opensource.org/licenses/ECL-2.0
  *
- *     Unless required by applicable law or agreed to in writing,
- *     software distributed under the License is distributed on an "AS IS"
- *     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- *     or implied. See the License for the specific language governing
- *     permissions and limitations under the License.
+ * Unless required under applicable law or agreed to in writing, software distributed under the
+ * License is distributed in an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for specific language governing permissions
+ * and limitations under the license.
+ *
  ******************************************************************************/
 
 package tds.exam.results.messaging;
